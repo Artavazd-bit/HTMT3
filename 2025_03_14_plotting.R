9707b73606e70c8c7ff847e8f7d761b541643d6b
Jason John <jason.berger@uni-wuerzburg.de>
2025-03-14 11:25:08 +0100

    added plot lines

--- 2025_03_14_plotting.R
+++ 2025_03_14_plotting.R
@@ -25,10 +25,12 @@ sim_overview_boot$Method <- "Boot"
 
 sim_overview_com <-  rbind(sim_overview_delta, sim_overview_boot)
 
-
 sim_overview_com$loading_com <- paste( "lmabda_1:", sim_overview_com$loading1, "and", "lambda_2:", sim_overview_com$loading2)
 
+ggplot(sim_overview_com, aes(n, RR_09)) + geom_line(aes(linetype = as.factor(sim_overview_com$Method))) + geom_point() + facet_grid(cols = vars(loading_com), rows = vars(correlation))  + geom_hline(yintercept = c(0.1, 0.8), color = "red")+ theme(legend.position="none")  
 
-ggplot(sim_overview_com, aes(n, RR_09)) + geom_line(aes(linetype = as.factor(sim_overview_com$Method))) + geom_point() + 
-  facet_grid(cols = vars(loading_com), rows = vars(correlation)) + theme(legend.position="none")
 
+ggplot(sim_overview_com, aes(n, RR_099)) + geom_line(aes(linetype = as.factor(sim_overview_com$Method))) + geom_point() + facet_grid(cols = vars(loading_com), rows = vars(correlation))  + geom_hline(yintercept = c(0.01, 0.8), color = "red")+ theme(legend.position="none")  
+
+
+ggplot(sim_overview_com, aes(n, RR_095)) + geom_line(aes(linetype = as.factor(sim_overview_com$Method))) + geom_point() + facet_grid(cols = vars(loading_com), rows = vars(correlation))  + geom_hline(yintercept = c(0.05, 0.8), color = "red")+ theme(legend.position="none")  
