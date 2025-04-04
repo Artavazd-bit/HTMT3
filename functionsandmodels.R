d474ff1308e41ef64dfd4c792e2eb863a3582a83
Jason John <jason.berger@uni-wuerzburg.de>
2025-04-04 10:16:53 +0200

    wrapped some functions into one function so the simulation is easier, included non normal data, cleaned up the code

--- functionsandmodels.R
+++ functionsandmodels.R
@@ -1,9 +1,7 @@
 ########################## Script for functions ################################
-library(lavaan)
-library(foreach)
 
 ###################### Gradient Calculation HTMT and HTMT2 #####################
-derivhtmt <- function(data, model, latent1, latent2, scale = TRUE, htmt2 = FALSE){
+derivhtmt <- function(data, model, latent1, latent2, scale, htmt2){
   
   model_df <- lavaanify(model)
   
@@ -55,17 +53,11 @@ derivhtmt <- function(data, model, latent1, latent2, scale = TRUE, htmt2 = FALSE
     print("ERROR")
   }
   
-  if (htmt2 == FALSE){
-  list(output = cor_values, HTMT = HTMT)
-  }else if (htmt2 == TRUE){
-  list(output = cor_values, HTMT2 = HTMT)
-  }else{
-    print("ERROR")
-  }
+list(output = cor_values, HTMT = HTMT)
 } 
 
 ########################## Calculation HTMT/HTMT2 ##############################
-calchtmt <- function(data, model, latent1, latent2, scale = TRUE, htmt2 = FALSE){
+calchtmt <- function(data, model, latent1, latent2, scale, htmt2){
   
   model_df <- lavaanify(model)
   
@@ -168,23 +160,136 @@ lavnacov <- function(data){
   NACOV <- base::crossprod(Zc) / nrow(data)
   NACOV
 }
+############################ Omega Correlation ################################
+calcovcor <- function(data) {
+  n <- nrow(data)
+  p <- ncol(data)
+  size_n <- (p * p - p) / 2
+  
+  # Pre-calculate means and centered data
+  data_means <- colMeans(data)
+  data_centered <- scale(data, center = TRUE, scale = FALSE)
+  data_sd <- apply(data, 2, sd)
+  
+  # Create indices for upper triangle
+  indices <- which(upper.tri(matrix(0, p, p)), arr.ind = TRUE)
+  
+  # Initialize result matrix
+  vc_r <- matrix(0, nrow = size_n, ncol = size_n)
+  
+  # Pre-calculate all possible products of centered variables
+  products <- array(0, dim = c(n, p, p))
+  for(i in 1:p) {
+    for(j in i:p) {
+      products[, i, j] <- data_centered[, i] * data_centered[, j]
+      products[, j, i] <- products[, i, j]
+    }
+  }
+  
+  
+  
+  # Function to get position in upper triangular matrix
+  get_pos <- function(i, j) {
+    if(i > j) {
+      temp <- i
+      i <- j
+      j <- temp
+    }
+    return(p*(i-1) - i*(i-1)/2 + j - i)
+  }
+  
+  # Calculate covariances using vectorized operations
+  for(idx1 in 1:nrow(indices)) {
+    x <- indices[idx1, 1]
+    y <- indices[idx1, 2]
+    
+    for(idx2 in idx1:nrow(indices)) {
+      z <- indices[idx2, 1]
+      t <- indices[idx2, 2]
+      
+      sd <- sd(data[,x]) * sd(data[,y]) * sd(data[,z]) * sd(data[,t])
+      
+      
+      # Calculate fourth-order moments using pre-computed products
+      mu_xyzt <- mean(products[, x, y] * products[, z, t])  / sd
+      
+      mu_xxzt <- mean(products[, x, x] * products[, z, t]) / sd
+      mu_yyzt <- mean(products[, y, y] * products[, z, t]) / sd
+      mu_xyzz <- mean(products[, x, y] * products[, z, z]) / sd
+      mu_xytt <- mean(products[, x, y] * products[, t, t]) / sd
+      
+      mu_xxtt <- mean(products[, x, x] * products[, t, t]) / sd
+      mu_xxzz <- mean(products[, x, x] * products[, z, z]) / sd
+      mu_yytt <- mean(products[, y, y] * products[, t, t]) / sd
+      mu_yyzz <- mean(products[, y, y] * products[, z, z]) / sd
+      
+      # Get correlation coefficients
+      rxy <- cor(data[, x], data[, y])
+      rzt <- cor(data[, z], data[, t])
+      
+      # Calculate covariance
+      cov_val <- (mu_xyzt - 0.5 * rxy * (mu_xxzt + mu_yyzt) - 
+                    0.5 * rzt * (mu_xyzz + mu_xytt) + 
+                    0.25 * rxy * rzt * (mu_xxzz + mu_xxtt + mu_yyzz + mu_yytt))
+      
+      pos1 <- get_pos(x, y)
+      pos2 <- get_pos(z, t)
+      
+      vc_r[pos1, pos2] <- cov_val
+      vc_r[pos2, pos1] <- cov_val  # Matrix is symmetric
+    }
+  }
+  
+  return(vc_r)
+}
 
-###########################Ultimate Function v3#################################
-doeverything <- function(data, model, alpha, latent1, latent2, scale = FALSE, htmt2 = FALSE)
+
+
+########################### Ultimate Function v3################################
+doeverything <- function(data, model, alpha, latent1, latent2, scale, htmt2)
 {
-  gdf <- derivhtmt(data = data, model = model, latent1 = latent1, latent2 = latent2, scale = scale, htmt2 = htmt)
+  starttime <- Sys.time()
+  gdf <- derivhtmt(data = data, model = model, latent1 = latent1, latent2 = latent2, scale = scale, htmt2 = htmt2)
   gradient <- as.matrix(gdf$output$gradient)
   if(scale == FALSE){
     omega <- calcovcov(data = data)
   } else if(scale == TRUE){
     omega <- calcovcor(data = data)
   } else print("ERROR")
-  se <- sqrt(t(gradient) %*% omega %*% gradient / nrow(data))
-  zvalue = (gdf$HTMT - 1)/se
+  se <- sqrt(t(gradient) %*% omega %*% gradient / nrow(data))[1]
+  zvalue <- (gdf$HTMT - 1)/se
+  endtime <- Sys.time()
+  tdelta <- endtime - starttime
+  # here i want to test whether im in the lowest alpha percent cases. 
+  ztest <- zvalue <  qnorm(p = alpha, mean = 0, sd = 1)
   
-  gdf$HTMT
-  zvalue
-  se
+  if (htmt2 == FALSE){
+    list(HTMT = gdf$HTMT, zval = zvalue, se = se, dec = ztest, time = tdelta)
+  }else if (htmt2 == TRUE){
+    list(HTMT2 = gdf$HTMT, zval = zvalue, se = se, dec = ztest, time = tdelta)
+  }else{
+    print("ERROR")
+  }
+}
+
+
+simFun <- function(data, model, alpha, latent1, latent2, scale = scale, htmt2 = htmt2, seed, bootruns)
+{
+  delta <- doeverything(data = data, model = model, alpha = alpha, latent1 = latent1, latent2 = latent2, scale = scale, htmt2 = htmt2)
+  
+  
+  set.seed(seed)
+  starttime <- Sys.time()
+  bootstrap <- boot(data_cfa, function(data, indices){calchtmt(data = data[indices,], model = model, latent1 = latent1, latent2 = latent2, scale = scale, htmt2 = htmt2)}, R = bootruns)
+  bootstrap_htmt_1_se <- sd(bootstrap$t, na.rm = TRUE)
+  #here its the other way, i want to test whether the upper bound is below zero
+  bootupperbound <- quantile(bootstrap$t, na.rm = TRUE, probs = 1-alpha)
+  endtime <- Sys.time()
+  tdeltaboot <- endtime - starttime
+  
+  bootdec <- bootupperbound < 1
+  
+  list(delta = delta, boot = list(upperbound = bootupperbound, dec = bootdec, time = tdeltaboot))
 }
 
 ############################# Models ###########################################
@@ -285,13 +390,21 @@ simModels
 
 ################################################################################
 
-model_est <- '
+model_est_old <- '
               #  latent variables
                 xi_1 =~ x11 + x12 + x13 + x14
                 xi_2 =~ x21 + x22 + x23 
                 
                 xi_1 ~~ xi_2
               ' 
+###############################################################################
 
+model_est<- '
+              #  latent variables
+                xi_1 =~ x11 + x12 + x13
+                xi_2 =~ x21 + x22 + x23 
+                
+                xi_1 ~~ xi_2
+              ' 
 
 
